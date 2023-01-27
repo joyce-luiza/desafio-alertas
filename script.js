6f7ab3034f131b6f6d3791a3e0a8b3e4a89c912c
joyce-luiza <joyceluizabrito@gmail.com>
2023-01-27 20:01:22 -0300

    adicionando a visualização de detalhes do alerta

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ const listAllAlerts = () => {
                             </svg>
                             Ver logs
                         </button>
-                        <button id="alert-details-${index}" class="button-secondary button-small">
+                        <button id="alert-details-${index}" class="button-secondary button-small" onclick="addAlertDetails(${index}); showAlertDetails(${index})">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
                                 <path fill="none" d="M0 0h24v24H0z" />
                                 <path d="M20 22H4a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1zm-1-2V4H5v16h14zM8 7h8v2H8V7zm0 4h8v2H8v-2zm0 4h5v2H8v-2z" "/></svg>
@@ -65,5 +65,96 @@ const listAllAlerts = () => {
         document.getElementById("cards").innerHTML += element;
     });
 };
+
+const addAlertDetails = (index) => {
+    let detailsEl = `
+                <!-- Modal content -->
+                <div id="view-alert" class="modal-content">
+                    <h1>Nome do alerta</h1>
+                    <span id="close-details-${index}" class="close">&times;</span>
+                    <label for="">Quando enviar os alertas</label>
+                    <p>${allAlerts[index].time}</p>
+                    <div class="toggle-switch">
+                        <label class="switch">
+                            ${
+                                alert.alerts
+                                    ? `<input id="switch-alerts-${index}" type="checkbox" checked>`
+                                    : `<input id="switch-alerts-${index}" type="checkbox">`
+                            }   
+                            <span class=" slider round"></span>
+                        </label>
+                        <p>Enviar alertas</p>
+                    </div>
+                    <label for="">Grupo de usuários a serem alertados</label>
+                    <p>${allAlerts[index].userGroups}</p>
+                    <label for="">Usuários a serem alertados</label>
+                    <p>${allAlerts[index].users}</p>
+                    <div id="view-alert--actions">
+                        <button class="button-secondary button-medium">
+                            <svg id="alert-edit-${index}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                <path fill="none" d="M0 0h24v24H0z" />
+                                <path d="M7.243 18H3v-4.243L14.435 2.322a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414L7.243 18zM3 20h18v2H3v-2z" />
+                            </svg>
+                            Editar alerta</button>
+                        <button class="button-secondary button-medium">
+                            <svg id="alert-delete-${index}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                <path fill="none" d="M0 0h24v24H0z" />
+                                <path d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zM9 4v2h6V4H9z" />
+                            </svg>
+                            Deletar alerta</button>
+                    </div>
+                </div>
+    `;
+
+    let modal = document.getElementById("alert-details");
+    modal.innerHTML += detailsEl;
+    modal.style.display = "block";
+
+    // // Get the <span> element that closes the modal
+    var span = document.getElementById(`close-details-${index}`);
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+        modal.innerHTML = "";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            modal.innerHTML = "";
+        }
+    };
+};
+
+const showAlertDetails = (index) => {};
+
+// /* Modal */
+// // Get the modal
+// var alertDetails = document.getElementById("alert-details");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function () {
+//     alertDetails.style.display = "block";
+// };
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//     alertDetails.style.display = "none";
+// };
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//     if (event.target == alertDetails) {
+//         alertDetails.style.display = "none";
+//     }
+// };
 
 window.onload = listAllAlerts();
